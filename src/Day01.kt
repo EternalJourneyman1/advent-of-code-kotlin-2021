af2d33c133c2f7d0e53c586343a5bc98107d1d75
Demetrious Robinson <demetriousr@demetriousr-a02.vmware.com>
2021-12-01 09:34:44 -0800

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+            input
+                    .map { it.toInt() }
+                    .countIncreases()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+           input
+                   .map { it.toInt() }
+                   .countDepthIncreases()
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01")
+    val inputIncreases = part1(testInput)
+    val inputDepthIncreases = part2(testInput)
+
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: ${part1(input)}")
+    check(inputIncreases == 1228)
+    println("Part 2: ${part2(input)}")
+    check(inputDepthIncreases == 1257)
+}
+
+fun List<Int>.countIncreases(): Int {
+    return mapIndexed { index, value ->
+        if (index != 0 && get(index - 1) < value) {
+            1
+        } else {
+            0
+        }
+    }.sum()
+}
+
+
+fun List<Int>.countDepthIncreases(): Int {
+    return windowed(3)
+            .map { it.sum() }
+            .windowed(2)
+            .count {
+                (a, b) -> a < b
+            }
 }
